6e150d9f80c998a394645343e9418105faa3eae5
¨Richard <creativo91@hotmail.com>
2023-08-23 16:15:39 -0400

    agregue Array para pedidos y nombres de cervezas permitidas

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,5 @@
-
-
-
+//  Array para almacenar los pedidos
+const pedidos = [];
 
 function iniciarRegistro() {
     const esMayor = confirm("Eres mayor de 18 años?");
@@ -12,14 +11,17 @@ function iniciarRegistro() {
     }
 }
 
-
 function registrarPedido() {
     const nombreProducto = document.getElementById("nombreProducto").value;
     const cantidad = parseInt(document.getElementById("cantidad").value);
     let precioUnitario = 990; // Precio unitario del producto
 
+    const productosPermitidos = ["Gletcher", "Gletcher Light", "Gletcher Dark "]; // Nombres de productos permitidos
+
     if (nombreProducto === '' || isNaN(cantidad) || cantidad <= 0) {
         alert('Por favor, ingresa datos válidos para el pedido.');
+    } else if (!productosPermitidos.includes(nombreProducto)) {
+        alert('Nombre de producto no válido. Solo se permiten los siguientes nombres: Gletcher, Gletcher Light, Gletcher Dark.');
     } else {
         let mensaje = `Has registrado un pedido de ${cantidad} unidades de ${nombreProducto}.`;
 
@@ -30,6 +32,14 @@ function registrarPedido() {
 
         const total = precioUnitario * cantidad;
         mensaje += ` El precio total es $${total}.`;
+
+        // detalles del pedido al array
+        const pedido = {
+            producto: nombreProducto,
+            cantidad: cantidad,
+            precioTotal: total
+        };
+        pedidos.push(pedido);
 
         alert(mensaje);
     }
